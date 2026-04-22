90295ea1c8ec92239b8379e262e175fc32794562
Tj-Bots <tjbots123@gmail.com>
2026-04-22 15:05:21 +0300

    Update sticker.py

--- Tj_Bots/extra/sticker.py
+++ Tj_Bots/extra/sticker.py
@@ -5,8 +5,8 @@ async def stickerid(bot, message):
     if message.reply_to_message and message.reply_to_message.sticker:
         sticker = message.reply_to_message.sticker
         await message.reply_text(
-            f"**מזהה סטיקר:**\n`{sticker.file_id}`\n\n**מזהה ייחודי:**\n`{sticker.file_unique_id}`",
+            f"**Sticker ID:**\n`{sticker.file_id}`\n\n**Unique ID:**\n`{sticker.file_unique_id}`",
             quote=True
         )
     else:
-        await message.reply_text("❌ אנא הגב על סטיקר עם הפקודה הזו.", quote=True)
+        await message.reply_text("❌ Please reply to a sticker with this command.", quote=True)
